1e5d84294b58d488982d708a1610541767f82cc5
Lou Zuniga <louzuniga86@gmail.com>
2019-05-14 00:31:55 -0700

    initial upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(document).ready(() => {
+  const typed = new Typed('.typed', {
+    strings: ['Software Engineer', 'Student', 'Enternal Learner'],
+    typeSpeed: 75,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+});
